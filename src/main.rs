176a6c37dca5eee073b715006882301d2eedffa7
Eric Olson <eric.rob.olson@gmail.com>
2021-06-05 17:44:05 -0700

    Got basic thing up and running.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use core::num;
 use image::GenericImageView;
 
 fn main() {
-    let loaded_img = image::open("test_pic.jpg").unwrap();
+    let loaded_img = image::open("test.jpg").unwrap();
     let (width, height) = loaded_img.dimensions();
 
     let mut turtler = Turtler::new(width, height);
@@ -122,6 +122,480 @@ fn main() {
                 }
             }
         }
+
+        for y in 0..height {
+            let [r0, g0, b0, a0] = loaded_img.get_pixel(x, y).0;
+            let [r1, g1, b1, a1] = turtler.output_buffer.get_pixel(x, y).0;
+
+            let r = ((r0 as u32 + r1 as u32) / 2) as u8;
+            let g = ((g0 as u32 + g1 as u32) / 2) as u8;
+            let b = ((b0 as u32 + b1 as u32) / 2) as u8;
+            turtler.draw_pixel(color(r, g, b), x, y);
+        }
+
+        for y in 0..height {
+            let [r0, g0, b0, a0] = loaded_img.get_pixel(x, y).0;
+            let [r1, g1, b1, a1] = turtler.output_buffer.get_pixel(x, y).0;
+
+            let r = (((r0 as u32) * 5 + r1 as u32) / 5) as u8;
+            let g = (((g0 as u32) * 5 + g1 as u32) / 5) as u8;
+            let b = (((b0 as u32) * 5 + b1 as u32) / 5) as u8;
+            turtler.draw_pixel(color(r, g, b), x, y);
+        }
+    }
+
+    // Normalish picture
+    for x in 0..width {
+        if x % line_w == 0 {
+            for y in 0..height {
+                if y % line_h == 0 {
+                    let mut count = 0;
+                    let mut avg_r = 0;
+                    let mut avg_g = 0;
+                    let mut avg_b = 0;
+
+                    // Calculate avg
+
+                    let x_range = x..x + line_w;
+                    let y_range = y..y + line_h;
+
+                    for x in x_range.clone() {
+                        for y in y_range.clone() {
+                            let x = x % turtler.width;
+                            let y = y % turtler.height;
+
+                            let [r, g, b, a] = loaded_img.get_pixel(x, y).0;
+                            avg_r += r as u32;
+                            avg_g += g as u32;
+                            avg_b += b as u32;
+                            count += 1;
+                        }
+                    }
+
+                    // Get average
+                    let avg_color = color(
+                        (avg_r / count) as u8,
+                        (avg_g / count) as u8,
+                        (avg_b / count) as u8,
+                    );
+
+                    for x in x_range.clone() {
+                        for y in y_range.clone() {
+                            if x % line_w < x_cutoff || y % line_h < y_cutoff {
+                                continue;
+                            }
+
+                            turtler.draw_pixel(avg_color, x, y);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let line_h = 2;
+    let line_w = 2;
+    for x in 0..width {
+        if x % line_w == 0 {
+            for y in 0..height {
+                if y % line_h == 0 {
+                    let mut count = 0;
+                    let mut avg_r = 0;
+                    let mut avg_g = 0;
+                    let mut avg_b = 0;
+
+                    // Calculate avg
+
+                    let x_range = x..x + line_w;
+                    let y_range = y..y + line_h;
+
+                    for x in x_range.clone() {
+                        for y in y_range.clone() {
+                            let x = x % turtler.width;
+                            let y = y % turtler.height;
+
+                            let [r, g, b, a] = loaded_img.get_pixel(x, y).0;
+                            avg_r += r as u32;
+                            avg_g += g as u32;
+                            avg_b += b as u32;
+                            count += 1;
+                        }
+                    }
+
+                    // Get average
+                    let avg_color = color(
+                        (avg_r / count) as u8,
+                        (avg_g / count) as u8,
+                        (avg_b / count) as u8,
+                    );
+
+                    for x in x_range.clone() {
+                        for y in y_range.clone() {
+                            if x % line_w < x_cutoff || y % line_h < y_cutoff {
+                                continue;
+                            }
+
+                            turtler.draw_pixel(avg_color, x, y);
+                        }
+                    }
+                }
+            }
+        }
+        // Do some really wack stuff now
+        for x in 0..width {
+            for y in 0..height {
+                let [r, g, b, a] = turtler.output_buffer.get_pixel(x, y).0;
+                let step_conversion = steps * (u8::MAX / steps.max(1));
+                let converted_r = u8::MAX - b;
+                let converted_g = u8::MAX - r;
+                let converted_b = u8::MAX - b;
+
+                if r > 0 {
+                    turtler.draw_pixel(color(converted_r, converted_g, converted_b), x, y);
+                }
+            }
+        }
+
+        // Do some other stuff
+        for x in 0..width {
+            if x % 2 != 0 {
+                for y in 0..height {
+                    if y % 2 != 0 {
+                        let [r, g, b, a] = loaded_img.get_pixel(x, y).0;
+                        let converted_r = u8::MAX - g;
+                        let converted_g = u8::MAX - b;
+                        let converted_b = u8::MAX - r;
+
+                        if r > 0 {
+                            turtler.draw_pixel(color(converted_r, converted_g, converted_b), x, y);
+                        }
+                    }
+                }
+            } else {
+                for y in 0..height {
+                    if y % 3 == 0 && y % 2 != 0 {
+                        let [r, g, b, a] = loaded_img.get_pixel(x, y).0;
+                        let converted_r = u8::MAX - b;
+                        let converted_g = u8::MAX - r;
+                        let converted_b = u8::MAX - g;
+                        turtler.draw_pixel(color(converted_r, converted_g, converted_b), x, y);
+                    }
+                }
+            }
+
+            for y in 0..height {
+                let [r, g, b, a] = turtler.output_buffer.get_pixel(x, y).0;
+
+                if r == 0 && (g == 0 || b == 0) {
+                    turtler.draw_pixel(color(255 / 3, 255 / 3, 255), x, y);
+                }
+            }
+        }
+    }
+
+    // Normalish picture
+    for x in 0..width {
+        if x % 2 == 0 {
+            continue;
+        }
+        for y in 0..height {
+            if y % 2 == 0 {
+                continue;
+            }
+            let (r, g, b, a) = {
+                if x % 3 == 0 || y % 3 == 0 {
+                    let [r, g, b, a] = loaded_img.get_pixel(x, y).0;
+                    (r, g, b, a)
+                } else {
+                    let [r0, g0, b0, a0] = loaded_img.get_pixel(x, y).0;
+                    let [r1, g1, b1, a1] = turtler.output_buffer.get_pixel(x, y).0;
+
+                    let r = (((r0 as u32) * 5 + r1 as u32) / 5) as u8;
+                    let g = (((g0 as u32) * 5 + g1 as u32) / 5) as u8;
+                    let b = (((b0 as u32) * 5 + b1 as u32) / 5) as u8;
+                    (r, g, b, a0)
+                }
+            };
+
+            turtler.draw_pixel(color(r, g, b), x, y);
+        }
+    }
+
+    for x in 0..width {
+        for y in 0..height {
+            let (r, g, b, a) = {
+                if x % 2 == 0 && y % 2 == 0 {
+                    let [r0, g0, b0, a0] = turtler.output_buffer.get_pixel(x, y).0;
+                    let [r1, g1, b1, a1] = loaded_img.get_pixel(x, y).0;
+
+                    let r = (((r0 as u32) * 5 + r1 as u32) / 5) as u8;
+                    let g = (((g0 as u32) * 5 + g1 as u32) / 5) as u8;
+                    let b = (((b0 as u32) * 5 + b1 as u32) / 5) as u8;
+                    (r, g, b, a0)
+                } else {
+                    let [r0, g0, b0, a0] = loaded_img.get_pixel(x, y).0;
+                    let [r1, g1, b1, a1] = turtler.output_buffer.get_pixel(x, y).0;
+
+                    let r = (((r0 as u32) * 5 + r1 as u32) / 5) as u8;
+                    let g = (((g0 as u32) * 5 + g1 as u32) / 5) as u8;
+                    let b = (((b0 as u32) * 5 + b1 as u32) / 5) as u8;
+                    (r, g, b, a0)
+                }
+            };
+
+            turtler.draw_pixel(color(r, g, b), x, y);
+        }
+    }
+
+    for x in 0..width {
+        for y in 0..height {
+            let (r, g, b, a) = {
+                {
+                    let [r0, g0, b0, a0] = loaded_img.get_pixel(x, y).0;
+                    let [r1, g1, b1, a1] = turtler.output_buffer.get_pixel(x, y).0;
+
+                    let r = (((r0 as u32) * 5 + r1 as u32) / 5) as u8;
+                    let g = (((g0 as u32) * 5 + g1 as u32) / 5) as u8;
+                    let b = (((b0 as u32) * 5 + b1 as u32) / 5) as u8;
+                    (r, g, b, a0)
+                }
+            };
+
+            turtler.draw_pixel(color(r, g, b), x + 5, y + 5);
+            turtler.draw_pixel(color(r, g, b), x - 5, y - 5);
+        }
+    }
+
+    // Normalish picture
+    for x in 0..width {
+        if x % line_w == 0 {
+            for y in 0..height {
+                if y % line_h == 0 {
+                    let mut count = 0;
+                    let mut avg_r = 0;
+                    let mut avg_g = 0;
+                    let mut avg_b = 0;
+
+                    // Calculate avg
+
+                    let x_range = x..x + line_w;
+                    let y_range = y..y + line_h;
+
+                    for x in x_range.clone() {
+                        for y in y_range.clone() {
+                            let x = x % turtler.width;
+                            let y = y % turtler.height;
+
+                            let [r, g, b, a] = loaded_img.get_pixel(x, y).0;
+                            avg_r += r as u32;
+                            avg_g += g as u32;
+                            avg_b += b as u32;
+                            count += 1;
+                        }
+                    }
+
+                    // Get average
+                    let avg_color = color(
+                        (avg_r / count) as u8,
+                        (avg_g / count) as u8,
+                        (avg_b / count) as u8,
+                    );
+
+                    for x in x_range.clone() {
+                        for y in y_range.clone() {
+                            if x % line_w < x_cutoff || y % line_h < y_cutoff {
+                                continue;
+                            }
+
+                            turtler.draw_pixel(avg_color, x, y);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Layer 1
+    for x in 0..width {
+        if x % line_w == 0 {
+            for y in 0..height {
+                if y % line_h == 0 {
+                    let mut count = 0;
+                    let mut avg_r = 0;
+                    let mut avg_g = 0;
+                    let mut avg_b = 0;
+
+                    // Calculate avg
+
+                    let x_range = x..x + line_w;
+                    let y_range = y..y + line_h;
+
+                    for x in x_range.clone() {
+                        for y in y_range.clone() {
+                            let x = x % turtler.width;
+                            let y = y % turtler.height;
+
+                            let [r, g, b, a] = loaded_img.get_pixel(x, y).0;
+                            avg_r += r as u32;
+                            avg_g += g as u32;
+                            avg_b += b as u32;
+                            count += 1;
+                        }
+                    }
+
+                    // Get average
+                    let avg_color = color(
+                        (avg_r / count) as u8,
+                        (avg_g / count) as u8,
+                        (avg_b / count) as u8,
+                    );
+
+                    for x in x_range.clone() {
+                        for y in y_range.clone() {
+                            if x % line_w < x_cutoff || y % line_h < y_cutoff {
+                                continue;
+                            }
+
+                            turtler.draw_pixel(avg_color, x, y);
+                        }
+                    }
+                }
+            }
+        }
+        // Do some really wack stuff now
+        for x in 0..width {
+            for y in 0..height {
+                let [r, g, b, a] = turtler.output_buffer.get_pixel(x, y).0;
+                let step_conversion = steps * (u8::MAX / steps.max(1));
+                let converted_r = u8::MAX - b;
+                let converted_g = u8::MAX - r;
+                let converted_b = u8::MAX - b;
+
+                if r > 0 {
+                    turtler.draw_pixel(color(converted_r, converted_g, converted_b), x, y);
+                }
+            }
+        }
+
+        // Do some other stuff
+        for x in 0..width {
+            if x % 2 != 0 {
+                for y in 0..height {
+                    if y % 2 != 0 {
+                        let [r, g, b, a] = loaded_img.get_pixel(x, y).0;
+                        let converted_r = u8::MAX - b;
+                        let converted_g = u8::MAX - r;
+                        let converted_b = u8::MAX - b;
+
+                        if r > 0 {
+                            turtler.draw_pixel(color(converted_r, converted_g, converted_b), x, y);
+                        }
+                    }
+                }
+            } else {
+                for y in 0..height {
+                    if y % 3 == 0 && y % 2 != 0 {
+                        let [r, g, b, a] = loaded_img.get_pixel(x, y).0;
+                        let converted_r = u8::MAX - b;
+                        let converted_g = u8::MAX - r;
+                        let converted_b = u8::MAX - b;
+                        turtler.draw_pixel(color(converted_r, converted_g, converted_b), x, y);
+                    }
+                }
+            }
+
+            for y in 0..height {
+                let [r, g, b, a] = turtler.output_buffer.get_pixel(x, y).0;
+
+                if r == 0 && (g == 0 || b == 0) {
+                    turtler.draw_pixel(color(255 / 3, 255 / 3, 255), x, y);
+                }
+            }
+        }
+
+        for y in 0..height {
+            let [r0, g0, b0, a0] = loaded_img.get_pixel(x, y).0;
+            let [r1, g1, b1, a1] = turtler.output_buffer.get_pixel(x, y).0;
+
+            let r = ((r0 as u32 + r1 as u32) / 2) as u8;
+            let g = ((g0 as u32 + g1 as u32) / 2) as u8;
+            let b = ((b0 as u32 + b1 as u32) / 2) as u8;
+            turtler.draw_pixel(color(r, g, b), x, y);
+        }
+
+        for y in 0..height {
+            let [r0, g0, b0, a0] = loaded_img.get_pixel(x, y).0;
+            let [r1, g1, b1, a1] = turtler.output_buffer.get_pixel(x, y).0;
+
+            let r = (((r0 as u32) * 42 + r1 as u32) / 45) as u8;
+            let g = (((g0 as u32) * 15 + g1 as u32) / 35) as u8;
+            let b = (((b0 as u32) * 25 + b1 as u32) / 25) as u8;
+            turtler.draw_pixel(color(r, g, b), x, y);
+        }
+    }
+
+    for x in 0..width {
+        for y in 0..height {
+            let [r, g, b, a] = turtler.output_buffer.get_pixel(x, y).0;
+            turtler.draw_pixel(color(u8::MAX - r, g, b), x, y);
+        }
+    }
+
+    for x in 0..width {
+        for y in 0..height {
+            let [r, g, b, a] = turtler.output_buffer.get_pixel(x, y).0;
+            turtler.draw_pixel(color(r, u8::MAX - g, b), x, y);
+        }
+    }
+
+    for x in 0..width {
+        for y in 0..height {
+            let [r, g, b, a] = turtler.output_buffer.get_pixel(x, y).0;
+
+            let cola = color(r, g, b);
+            let colb = color(u8::MAX - r, u8::MAX - g, b);
+
+            let [r0, g0, b0, a0] = cola.0;
+            let [r1, g1, b1, a1] = colb.0;
+
+            let r = (((r0 as u32) * 42 + r1 as u32) / 45) as u8;
+            let g = (((g0 as u32) * 15 + g1 as u32) / 35) as u8;
+            let b = (((b0 as u32) * 25 + b1 as u32) / 25) as u8;
+            turtler.draw_pixel(color(r, g, b), x, y);
+        }
+    }
+
+    for x in 0..width {
+        if x % 4 == 0 {
+            for y in 0..height {
+                if y % 4 == 0 {
+                    let [r, g, b, a] = loaded_img.get_pixel(x, y).0;
+
+                    let cola = color(r, g, b);
+                    let colb = color(u8::MAX - r, u8::MAX - g, b);
+
+                    let [r0, g0, b0, a0] = cola.0;
+                    let [r1, g1, b1, a1] = colb.0;
+
+                    let r = (((r0 as u32) * 42 + r1 as u32) / 45) as u8;
+                    let g = (((g0 as u32) * 15 + g1 as u32) / 35) as u8;
+                    let b = (((b0 as u32) * 25 + b1 as u32) / 25) as u8;
+                    turtler.draw_pixel(color(r, g, b), x, y);
+                }
+            }
+        }
+    }
+
+    // Shade it
+    for x in 0..width {
+        for y in 0..height {
+            if x % 4 != 0 && y % 4 != 0 {
+                continue;
+            }
+            let [r, g, b, a] = loaded_img.get_pixel(x, y).0;
+
+            turtler.draw_pixel(color(r, g, b), x, y);
+        }
     }
 
     // Compositor - this should enable creating 'fractally' images
@@ -163,6 +637,9 @@ impl Turtler {
     }
 
     pub fn draw_pixel(&mut self, color: Color, x: u32, y: u32) {
+        let x = x % self.width;
+        let y = y % self.height;
+
         *self.output_buffer.get_pixel_mut(x, y) = color;
     }
 
@@ -171,7 +648,7 @@ impl Turtler {
         let mut y = y0;
         for l in 0..len {
             for _ in 0..slope {
-                *self.output_buffer.get_pixel_mut(x, y) = color;
+                self.draw_pixel(color, x, y);
 
                 y = self.increment_y(y);
             }
